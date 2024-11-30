cf19885edbda2207d6419cbebd24e949a659da3b
tomlrd <thomas.laroudie@gmail.com>
2024-11-30 20:15:38 +0100

    rez

--- src/renderer/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import 'react-tabs/style/react-tabs.css'
-import './assets/index.css'
-import App from './App'
-import '../../../node_modules/react-grid-layout/css/styles.css'
-import '../../../node_modules/react-resizable/css/styles.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
--- src/renderer/src/store/index.ts
+++
@@ -1,5 +0,0 @@
-import { useLayoutsStore } from './layoutStore'
-import { useKeyBindsStore } from './keybindsStore'
-import { useOptionsStore } from './optionsStore'
-
-export { useLayoutsStore, useKeyBindsStore, useOptionsStore }
